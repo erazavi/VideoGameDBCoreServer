15574d67f2b6c45b5e0451b14e9cfc415005d9f6
erazavi <elijah.razavi@live.com>
2020-12-10 23:49:12 -0800

    Added authentication to view model controller route

--- DeliveryCoreServer/Controllers/PublisherWithVideoGamesController.cs
+++ DeliveryCoreServer/Controllers/PublisherWithVideoGamesController.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace DeliveryCoreServer.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class PublisherWithVideoGamesController : ControllerBase
